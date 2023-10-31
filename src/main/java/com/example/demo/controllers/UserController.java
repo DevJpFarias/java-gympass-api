2076dffb294c832f095d173a5398170382acb437
DevJpFarias <joaopfc2764@gmail.com>
2023-10-31 16:38:28 -0300

    feat: criado controller de usuários

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import com.example.demo.domain.user.User;
+import com.example.demo.domain.user.UsersRepository;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+  @Autowired
+  private UsersRepository repository;
+
+  @GetMapping
+  public ResponseEntity<List<User>> getAllUsers() {
+    var users = repository.findAll();
+
+    return ResponseEntity.ok(users);
+  }
+}
