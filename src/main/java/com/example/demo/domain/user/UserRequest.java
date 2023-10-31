bea17d6b98c9a0fdcc25140454fa23844a838bdd
DevJpFarias <joaopfc2764@gmail.com>
2023-10-31 16:06:56 -0300

    feat: criada interface de dados do usuário

---
+++ src/main/java/com/example/demo/domain/user/UserRequest.java
@@ -0,0 +1,19 @@
+package com.example.demo.domain.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Min;
+
+public record UserRequest(
+  String id,
+
+  @NotBlank
+  String name,
+
+  @NotBlank @Email
+  String email,
+
+  @NotBlank @Min(6)
+  String password
+) {
+}
