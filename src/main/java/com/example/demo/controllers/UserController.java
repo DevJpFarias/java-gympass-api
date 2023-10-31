b0ce4496d79245fffac5448c24b460c9c2d4948d
DevJpFarias <joaopfc2764@gmail.com>
2023-10-31 20:00:31 -0300

    refactor: retirando responsabilidades do controller e implementando services

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -3,25 +3,41 @@ package com.example.demo.controllers;
 import java.util.List;
 
 import com.example.demo.domain.user.User;
-import com.example.demo.domain.user.UsersRepository;
+import com.example.demo.domain.user.UserRequest;
+import com.example.demo.services.CreateUserService;
+import com.example.demo.services.ListAllUsersService;
+
+import jakarta.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
   @Autowired
-  private UsersRepository repository;
+  private ListAllUsersService listAllUsersService;
+
+  @Autowired
+  private CreateUserService createUserService;
 
   @GetMapping
   public ResponseEntity<List<User>> getAllUsers() {
-    var users = repository.findAll();
+    var users = listAllUsersService.execute();
 
     return ResponseEntity.ok(users);
+  }
+
+  @PostMapping
+  public ResponseEntity<String> createUser(@RequestBody @Valid UserRequest data) {
+    createUserService.execute(data);
+
+    return ResponseEntity.ok().build();
   }
 }
