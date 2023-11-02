501e33338f904fae24fa46e3a56bb89fad6005dc
DevJpFarias <joaopfc2764@gmail.com>
2023-11-02 00:22:57 -0300

    feat: retirando método não usado do repositório

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -1,11 +1,7 @@
 package com.example.demo.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.domain.user.User;
 
-public interface UserRepository extends JpaRepository<User, String> {
-  List<User> findAll();
-}
+public interface UserRepository extends JpaRepository<User, String> {}
