6c6139502793ca4e57d8b365a4a9d21e4ec318aa
DevJpFarias <joaopfc2764@gmail.com>
2023-11-03 21:55:11 -0300

    refactor: mudando nome do repositório no service

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -13,29 +13,29 @@ import com.example.demo.repositories.UserRepository;
 @Service
 public class UserService {
   @Autowired
-  UserRepository repository;
+  UserRepository userRepository;
 
   public User createUser(UserRequest data) {
     User newUser = new User(data);
     
-    repository.save(newUser);
+    userRepository.save(newUser);
 
     return newUser;
   }
 
   public List<User> getAllUsers() {
-    var users = repository.findAll();
+    var users = userRepository.findAll();
 
     return users;
   }
 
   public Optional<User> findUserById(String id) {
-    var user = repository.findById(id);
+    var user = userRepository.findById(id);
 
     return user;
   }
 
   public User updateUser(User user) {
-    return repository.save(user);
+    return userRepository.save(user);
   }
 }
