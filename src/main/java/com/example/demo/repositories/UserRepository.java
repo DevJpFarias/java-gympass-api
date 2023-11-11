916dd23771c2e9e515a10296852aa87556763d14
DevJpFarias <joaopfc2764@gmail.com>
2023-11-11 01:54:43 -0300

    feat: criado método para encontrar usuário pelo email

--- src/main/java/com/example/demo/repositories/UserRepository.java
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -1,7 +1,9 @@
 package com.example.demo.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.domain.user.User;
 
-public interface UserRepository extends JpaRepository<User, String> {}
+public interface UserRepository extends JpaRepository<User, String> {
+  Optional<User> findByEmail(String email);
+}
