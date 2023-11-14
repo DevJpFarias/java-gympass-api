ddfa1b0aace2717e168fea3aff7ae7fb8e656e17
DevJpFarias <joaopfc2764@gmail.com>
2023-11-13 22:12:53 -0300

    feat: criado service para autenticação

---
+++ src/main/java/com/example/demo/services/AuthorizationService.java
@@ -0,0 +1,18 @@
+package com.example.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.example.demo.repositories.UserRepository;
+
+public class AuthorizationService implements UserDetailsService {
+  @Autowired
+  UserRepository repository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return repository.findByLogin(username);
+  }
+}
