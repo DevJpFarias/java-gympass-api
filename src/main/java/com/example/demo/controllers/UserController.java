df8326c2cbdd3e15d8fb58e8f3882c873ef776c8
DevJpFarias <joaopfc2764@gmail.com>
2023-11-14 19:36:22 -0300

    refactor: melhorando anotação do controller

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,8 +40,8 @@ public class UserController {
     return ResponseEntity.status(HttpStatus.CREATED).body(user);
   }
 
-  @GetMapping(":id")
-  public ResponseEntity getUserProfile(String id) throws Exception {
+  @GetMapping("/{id}")
+  public ResponseEntity getUserProfile(@PathVariable String id) throws Exception {
     try {
       User user = userService.getUserProfile(id);
 
