83e6d190c4482dec5a896fc0e6e8e30b2debc749
DevJpFarias <joaopfc2764@gmail.com>
2023-11-16 20:19:47 -0300

    refactor: faltavam alguns dados no record

--- src/main/java/com/example/demo/domain/checkin/CheckInRequest.java
+++ src/main/java/com/example/demo/domain/checkin/CheckInRequest.java
@@ -2,7 +2,14 @@ package com.example.demo.domain.checkin;
 
 import java.time.LocalDateTime;
 
+import com.example.demo.domain.gym.Gym;
+import com.example.demo.domain.user.User;
+
 public record CheckInRequest(
-  LocalDateTime validated_at
+  LocalDateTime validated_at,
+
+  User user,
+
+  Gym gym
 ) {
 }
